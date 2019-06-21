c8be06b7779307647550b3af10defd4683981049
Ricardo Viera <Ricardo.Viera@nycha.nyc.gov>
2019-06-21 12:24:05 -0400

    develop rv- added viewmodel and connected to fragment

--- app/src/main/java/com/example/rv193/mvvmudemy/home/ListFragment.java
+++ app/src/main/java/com/example/rv193/mvvmudemy/home/ListFragment.java
@@ -1,6 +1,11 @@
 package com.example.rv193.mvvmudemy.home;
 
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -10,11 +15,14 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.rv193.mvvmudemy.R;
+import com.example.rv193.mvvmudemy.viewmodel.ListViewModel;
 
 public class ListFragment extends Fragment {
-    RecyclerView recyclerView;
-    TextView textView;
-    ProgressBar progressBar;
+    private RecyclerView recyclerView;
+    private TextView textView;
+    private ProgressBar progressBar;
+    private ListViewModel viewModel;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -28,4 +36,41 @@ public class ListFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+//        MVVM will reuse one instance of this !!!
+        viewModel = ViewModelProviders.of(this).get(ListViewModel.class);
+//        We need to listen to all of the livedata objects with the following observe method
+        observeViewModel();
+    }
+
+    private void observeViewModel() {
+//        to observe livedata use the GETTER from ViewModel and Call OBSERVE
+//        viewmodel.getYouCreated().observe(LifeCycleOwner: this, anonymous f() lambda)
+        viewModel.getRepos().observe(this, repos -> {
+            if(repos != null) {
+                recyclerView.setVisibility(View.VISIBLE);
+            }
+        });
+//        reference to the error live data and do the same thing
+        viewModel.getError().observe(this, isError -> {
+//            if isError Check
+            if(isError) {
+                textView.setVisibility(View.VISIBLE);
+                recyclerView.setVisibility(View.GONE);
+                textView.setText(R.string.api_error_repos);
+            } else {
+                textView.setVisibility(View.GONE);
+                textView.setText(null);
+            }
+        });
+//        Loading live data
+        viewModel.getLoading().observe(this, isLoading -> {
+            progressBar.setVisibility(isLoading ? View.VISIBLE : View.GONE);
+            if(isLoading) {
+                textView.setVisibility(View.GONE);
+                recyclerView.setVisibility(View.GONE);
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/rv193/mvvmudemy/viewmodel/ListViewModel.java
+++ app/src/main/java/com/example/rv193/mvvmudemy/viewmodel/ListViewModel.java
@@ -1,13 +1,73 @@
 package com.example.rv193.mvvmudemy.viewmodel;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 
 import com.example.rv193.mvvmudemy.model.Repo;
+import com.example.rv193.mvvmudemy.network.RepoApi;
 
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ListViewModel extends ViewModel {
     private final MutableLiveData<List<Repo>> repos = new MutableLiveData<>();
     private final MutableLiveData<Boolean> repoLoadError = new MutableLiveData<>();
+    private final MutableLiveData<Boolean> loading = new MutableLiveData<>();
+
+// Fetch Repos() will handle the data coming from API
+    private Call<List<Repo>> repoCall;
+
+    public ListViewModel() {
+        fetchRepos();
+    }
+
+//    expose all of our live data with getters
+//    use LiveData so that the view can not edit it
+    public LiveData<List<Repo>> getRepos() {
+        return repos;
+    }
+
+    public LiveData<Boolean> getError() {
+        return repoLoadError;
+    }
+
+    public LiveData<Boolean> getLoading() {
+        return loading;
+    }
+
+    private void fetchRepos() {
+        loading.setValue(true);
+        repoCall = RepoApi.getInstance().getRepositories();
+//        we can call enqueue and get anonymous type callback
+        repoCall.enqueue(new Callback<List<Repo>>() {
+            @Override
+            public void onResponse(Call<List<Repo>> call, Response<List<Repo>> response) {
+                repoLoadError.setValue(false);
+                repos.setValue(response.body());
+                loading.setValue(false);
+                repoCall = null;
+            }
+
+            @Override
+            public void onFailure(Call<List<Repo>> call, Throwable t) {
+                repoLoadError.setValue(true);
+                loading.setValue(false);
+                repoCall = null;
+            }
+        });
+    }
+
+//    onCleared method is for viewmodel method
+
+    @Override
+    protected void onCleared() {
+        if (repoCall != null) {
+            repoCall.cancel();
+            repoCall = null;
+        }
+    }
 }
